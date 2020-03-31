f454c75c62558378eb0d92fb378e59573e00d954
Simon 'Sze' L. Schlee <schlee.simon@gmail.com>
2020-03-31 22:38:16 +0200

    added kabellaengeToRPEmax, check pruefung_id exists

--- application/controllers/Pruefung.php
+++ application/controllers/Pruefung.php
@@ -67,11 +67,50 @@ class Pruefung extends CI_Controller {
             $pruefung_id = $this->Pruefung_model->new(array('gid'=>$gid));
             redirect('pruefung/edit/'.$pruefung_id);
         } else {
+            // $this->load->view('templates/header');
+            // TODO check how show_error works
             show_error('Gerät mit der id "'.$gid.'" existiert nicht.', 404);
+            // $this->load->view('templates/footer');
         }
 	}
 
+    private function getGid($pruefung_id) {
+        $pruefung = $this->Pruefung_model->get($pruefung_id);
+        return $pruefung['gid'];
+    }
+
+    /*Verlängerungskabel
+      Laut Norm darf der RPE (schutzleiter)
+      0,3 Ohm für die ersten 5m betragen
+      für jede weiteren 7,5m 0,1 Ohm mehr
+      maximal jedoch 1 Ohm.*/
+    // sze: TODO check by dante
+    // sze: TODO add unit tests for this function
+    // sze: check(0.3, kabellaengeToRPEmax(1))
+    // sze: check(0.3, kabellaengeToRPEmax(5))
+    // sze: check(0.4, kabellaengeToRPEmax(12.5))
+    // sze: check(0.5, kabellaengeToRPEmax(20))
+    // sze: check(1,   kabellaengeToRPEmax(1000000))
+    private function kabellaengeToRPEmax($kabellaenge) {
+        $first_kl = 5;
+        $first = min($kabellaenge, $first_kl);
+        $rest  = max(0, $kabellaenge - $first_kl);
+        $rest_ohm = 0.1;
+        $rest_ratio = 1 / 7.5;
+
+        $rpe_max = min(0.3 + $rest * $rest_ratio * $rest_ohm, 1);
+        return $rpe_max;
+    }
+
     function edit($pruefung_id) {
+        if(!$this->Pruefung_model->get($pruefung_id)) {
+            // $this->load->view('templates/header');
+            // TODO check how show_error works
+            show_error('Prüfung mit der id "'.$pruefung_id.'" existiert nicht.', 404);
+            // $this->load->view('templates/footer');
+            return NULL;
+        }
+
 		$felder = array('datum','mid','pid','sichtpruefung','schutzleiter','isowiderstand','schutzleiterstrom','beruehrstrom','funktion','bestanden','bemerkung');
 
 		$this->form_validation->set_rules('mid', 'Messgerät', 'callback_mid_check');
