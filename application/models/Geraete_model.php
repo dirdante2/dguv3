b699a71535de4fd5501499068456206cab1008ed
Simon 'Sze' L. Schlee <schlee.simon@gmail.com>
2020-04-02 23:00:48 +0200

    bestanden feld und pruefer left join

--- application/models/Geraete_model.php
+++ application/models/Geraete_model.php
@@ -15,11 +15,11 @@ class Geraete_model extends CI_Model {
 	}
 
 	function get($gid=NULL) {
-		$this->db->select('geraete.*, orte.name AS ortsname, pruefung.datum AS letztesdatum, (select count(*) from pruefung as pr where geraete.gid = pr.gid) AS anzahl, pruefer.name as pruefername');
+		$this->db->select('geraete.*, orte.name AS ortsname, pruefung.bestanden, pruefung.datum AS letztesdatum, (select count(*) from pruefung as pr where geraete.gid = pr.gid) AS anzahl, pruefer.name as pruefername');
 		$this->db->from('geraete');
 		$this->db->join('orte', 'geraete.oid = orte.oid');
 		$this->db->join('pruefung','pruefung.gid = geraete.gid AND pruefung.pruefungid = (SELECT pruefungid from pruefung as pr where geraete.gid = pr.gid order by datum desc, pruefungid desc limit 1)','LEFT');
-		$this->db->join('pruefer', 'pruefer.pid = pruefung.pid');
+		$this->db->join('pruefer', 'pruefer.pid = pruefung.pid', 'LEFT');
 
 		if($gid===NULL) {
 			return $this->db->get()->result_array();
@@ -35,11 +35,11 @@ class Geraete_model extends CI_Model {
 	}
 
 	function getByOid($oid) {
-		$this->db->select('geraete.*, orte.name AS ortsname, pruefung.datum AS letztesdatum, (select count(*) from pruefung as pr where geraete.gid = pr.gid) AS anzahl, pruefer.name as pruefername');
+		$this->db->select('geraete.*, orte.name AS ortsname, pruefung.bestanden, pruefung.datum AS letztesdatum, (select count(*) from pruefung as pr where geraete.gid = pr.gid) AS anzahl, pruefer.name as pruefername');
 		$this->db->from('geraete');
 		$this->db->join('orte', 'geraete.oid = orte.oid');
 		$this->db->join('pruefung','pruefung.gid = geraete.gid AND pruefung.pruefungid = (SELECT pruefungid from pruefung as pr where geraete.gid = pr.gid order by datum desc, pruefungid desc limit 1)','LEFT');
-		$this->db->join('pruefer', 'pruefer.pid = pruefung.pid');
+		$this->db->join('pruefer', 'pruefer.pid = pruefung.pid', 'LEFT');
 		$this->db->where('orte.oid',$oid);
 		return $this->db->get()->result_array();
 	}
