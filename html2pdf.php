9ef34e95cd5dc11d5179700f4d332044fc0cd7a4
dirdante2 <61091766+dirdante2@users.noreply.github.com>
2020-04-10 19:41:37 +0200

    html2pdf hinzugefügt

---
+++ html2pdf.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+use Spipu\Html2Pdf\Html2Pdf;
+
+
+$html2pdf = new HTML2PDF("P","A4","de", true, "UTF-8", array(20, 10, 20, 10));
+
+$html2pdf->writeHTML('<h1>HelloWorld</h1>This is my first test');
+$html2pdf->output(__DIR__.'/pdf/file_xxxx.pdf', 'F');
+
+?>
