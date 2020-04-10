54bf33bb4de76fa147b1de29bbb35343455cc80b
dirdante2 <61091766+dirdante2@users.noreply.github.com>
2020-04-10 20:05:56 +0200

    Update html2pdf.php

--- html2pdf.php
+++ html2pdf.php
@@ -4,11 +4,15 @@ require __DIR__.'/vendor/autoload.php';
 
 use Spipu\Html2Pdf\Html2Pdf;
 
-
+//protokoll portrait = P
+//übersicht landscape = L
 $html2pdf = new HTML2PDF("P","A4","de", true, "UTF-8", array(20, 10, 20, 10));
 
+//var
 $html2pdf->writeHTML('<h1>HelloWorld</h1>This is my first test');
 
+//dateipfad protokoll= /pdf/<prüfung_jahr>/<ortsname>/<geräteID>_<prüfungID>_<gerätename>.pdf
+//dateipfad ortsübersicht /pdf/<jahr>/<ortsname>/übersicht.pdf
 $html2pdf->output(__DIR__.'/pdf/file_xxxx.pdf', 'F');
 
 ?>
