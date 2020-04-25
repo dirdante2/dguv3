f5c8289f74d781e0f36f2bc16a576f7c137520db
Simon 'Sze' L. Schlee <schlee.simon@gmail.com>
2020-04-25 18:54:02 +0200

    cleanup geraete controller endpoints

--- application/controllers/Geraete_json.php
+++ application/controllers/Geraete_json.php
@@ -20,32 +20,22 @@ class Geraete_json extends CI_Controller {
 		$this->form_validation->set_error_delimiters('<div class="alert alert-danger">', '</div>');
 	}
 
-
-	//output geraete/uebersicht/$oid als json format
-	function json($oid="") {
-
+	private function data($oid="") {
 		$data['ort'] = $this->Orte_model->get($oid);
 		$data['geraete'] = $this->Geraete_model->getByOid($oid);
 		$data['dguv3_show_geraete_col']= $this->config->item('dguv3_show_geraete_pdf_col');
-
-
 		$data['dguv3_logourl']= $this->config->config['base_url'].$this->config->item('dguv3_logourl');
 		$data['qrcode']= 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data='.$this->config->config['base_url'].'/index.php/geraete/index/'.$oid;
+		return $data;
+	}
 
+	//output geraete/uebersicht/$oid als json format
+	function genpdf($oid="") {
+		$data = $this->data($oid);
 
 		//var die nicht in json nötig sind
 		$firma_id = $data['ort']['orte_firmaid'];
 		unset($data['ort']['orte_firmaid']);
-		//unset($data['ort']['firmen_firmaid']);
-		//json ausgabe ob eingeloggt
-		if($this->session->userdata('level')){
-			$data['login']['username']= $this->session->userdata('username');
-			$data['login']['level']=$this->session->userdata('level');
-		} else {
-			$data['login']['username']=null;
-			$data['login']['level']=null;
-		}
-		//echo json_encode($data);
 
 		$year = date("Y");
 		$ortsname = $data['ort']['name'];
@@ -54,35 +44,9 @@ class Geraete_json extends CI_Controller {
 		echo $filename;
 	}
 
-
-
-	function json2($oid="") {
-
-		$data['ort'] = $this->Orte_model->get($oid);
-		$data['geraete'] = $this->Geraete_model->getByOid($oid);
-		$data['dguv3_show_geraete_col']= $this->config->item('dguv3_show_geraete_pdf_col');
-
-
-		$data['dguv3_logourl']= $this->config->config['base_url'].$this->config->item('dguv3_logourl');
-		$data['qrcode']= 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data='.$this->config->config['base_url'].'/index.php/geraete/index/'.$oid;
-
-
-		//var die nicht in json nötig sind
-		$firma_id = $data['ort']['orte_firmaid'];
-		unset($data['ort']['orte_firmaid']);
-		//unset($data['ort']['firmen_firmaid']);
-		//json ausgabe ob eingeloggt
-		if($this->session->userdata('level')){
-			$data['login']['username']= $this->session->userdata('username');
-			$data['login']['level']=$this->session->userdata('level');
-		} else {
-			$data['login']['username']=null;
-			$data['login']['level']=null;
-		}
+	function json($oid="") {
+		$data = $this->data($oid);
 		echo json_encode($data);
-
-
 	}
-
 
 }
