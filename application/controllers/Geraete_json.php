8a55cf1cdd4abec6e420183ef773b30543e726c9
Simon 'Sze' L. Schlee <schlee.simon@gmail.com>
2020-04-25 20:13:58 +0200

    geraete_json and pruefung_json pdfgen changes

--- application/controllers/Geraete_json.php
+++ application/controllers/Geraete_json.php
@@ -26,20 +26,26 @@ class Geraete_json extends CI_Controller {
 		$data['dguv3_show_geraete_col']= $this->config->item('dguv3_show_geraete_pdf_col');
 		$data['dguv3_logourl']= $this->config->config['base_url'].$this->config->item('dguv3_logourl');
 		$data['qrcode']= 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data='.$this->config->config['base_url'].'/index.php/geraete/index/'.$oid;
+
+		// generate filename
+		$year = date("Y");
+		$ortsname = $data['ort']['name'];
+		$firma_id = $data['ort']['orte_firmaid'];
+		$filename = 'pdf/'.$firma_id.'/'.$year.'/'.$ortsname.'_'.$oid.'/'.$ortsname.'_liste.pdf';
+		$data['filename'] = $filename;
+
+		//var die nicht in json nötig sind
+		unset($data['ort']['orte_firmaid']);
+
 		return $data;
 	}
 
 	//output geraete/uebersicht/$oid als json format
 	function genpdf($oid="") {
 		$data = $this->data($oid);
+		$filename = $data['filename'];
+		unset($data['filename']);
 
-		//var die nicht in json nötig sind
-		$firma_id = $data['ort']['orte_firmaid'];
-		unset($data['ort']['orte_firmaid']);
-
-		$year = date("Y");
-		$ortsname = $data['ort']['name'];
-		$filename = 'pdf/'.$firma_id.'/'.$year.'/'.$ortsname.'_'.$oid.'/'.$ortsname.'_liste.pdf';
 		$this->Pdf_model->generate_pdf('uebersicht', $data, $filename);
 		echo $filename;
 	}
